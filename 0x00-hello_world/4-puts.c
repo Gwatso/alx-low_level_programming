2b2898d7b75537b623d0bbb3882e0af511284de7
Gwatso <nysongwatso@gmail.com>
2023-03-18 05:08:49 -0700

    C program that prints a text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*  main - Entry
+ *  Return: Always 0
+
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+>>>>>>> e82da641b13241a994a311aa96c381f6fc9aedc3
+ */
+int main(void)
+{
+
+puts("\"Programmingis like building a multilingual puzzle");
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
