bc0b93627fbafde7f6ebd354afded2ba11b53631
Gwatso <nysongwatso@gmail.com>
2023-03-18 05:30:13 -0700

    Cprogram that prints a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 /**
-
  *  main - Entry
  *  Return: Always 0
-
  * main - A program that prints a line with puts function
- * Return: 0 (Success)
-
  */
+
 int main(void)
 {
 
 puts("\"Programmingis like building a multilingual puzzle");
-
-puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
