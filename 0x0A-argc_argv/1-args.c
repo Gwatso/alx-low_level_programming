db9721c0beb747e94cb23a79c332f1610df510b0
Gwatso <nysongwatso@gmail.com>
2023-04-10 11:19:45 -0700

    rite a program that prints the number of arguments passed into it.
    Your program should print a number, followed by a new line

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,10 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-	printf("argv[%d]= %s\n", i, argv[i]);
-}
-return (0);
+
+(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
 }
